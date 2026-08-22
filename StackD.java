cd698ba05b48265d7b758ac58b0cdf2d5dd8fc2a
muslkanjha001 <muskanjha232@gmail.com>
2026-08-22 12:41:27 +0530

    StackD.java

---
+++ StackD.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class StackD{
+    public static void main(String args[]){
+        int arr[] = {6,10,2,4,1};
+        Stack<Integer> s = new Stack<>();
+        int nxtGreater[] = new int[arr.length];
+        for(int i=arr.length-1; i>=0; i--){
+            //1 while
+            while(!s.isEmpty() && arr[s.peek()]<=arr[i]){
+                s.pop();
+
+            }
+            //2 if-else
+                if(s.isEmpty()){
+                    nxtGreater[i] = -1;
+                }
+                else {
+                    nxtGreater[i] = arr[s.peek()];
+                }
+                                
+            // push in 
+               s.push(i);
+            }
+               for(int i=0; i<nxtGreater.length; i++){
+                      System.out.print(nxtGreater[i]+" ");
+               
+               }
+               System.out.println();
+            }
+
+
+
+        }
+
+    
+
+
