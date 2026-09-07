2e71a6169c2b9199592eddb2cf3662b0edd573de
muslkanjha001 <muskanjha232@gmail.com>
2026-09-07 22:10:58 +0530

    Queu4.java non repating character

---
+++ Que4.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Que4{
+    public static void printNonRepeating(String str){
+        int freq[] = new int[26]; // 'a'-'Z'
+        Queue<Character> q = new LinkedList<>();
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a']>1){
+                q.remove();
+            }
+            if(q.isEmpty()){
+                System.out.print(-1+" ");
+
+            }
+            else {
+                System.out.print(q.peek()+" ");
+            }
+
+            }
+            System.out.println();
+         }
+         public static void main(String args[]){
+            String str = "aabccxb";
+            printNonRepeating(str);
+
+         }
+    }
+
+
+
+
+ 
+            
+             
+ 
